3dae30a370b058332f1ea6834e1337f5ba987fd2
wooov <wooovi@126.com>
2023-09-13 20:13:15 +0800

    新增商品api

--- src/api/goods.js
+++ src/api/goods.js
@@ -38,3 +38,70 @@ export const releaseOn = (goodsId) => {
         }
     })
 }
+//用户删除商品
+export const del = (goodsId) => {
+    return request({
+        url: baseurl + "/",
+        method: "delete",
+        headers: headers,
+        data: {
+            goodsId: goodsId
+        }
+    })
+}
+
+//管理员冻结商品
+export const admin_freeze = (goodsId) => {
+    return request({
+        url: baseurl + "/admin/freeze",
+        method: "post",
+        headers: headers,
+        data: {
+            goodsId: goodsId
+        }
+    })
+}
+//管理员解冻商品
+export const admin_unfreeze = (goodsId) => {
+    return request({
+        url: baseurl + "/admin/unfreeze",
+        method: "post",
+        headers: headers,
+        data: {
+            goodsId: goodsId
+        }
+    })
+}
+//游客获取单个商品信息
+export const getInfo = (goodsId) => {
+    return request({
+        url: baseurl + "/info/" + goodsId,
+        method: "get"
+    })
+}
+//管理员分页查询商品列表
+export const admin_page = (data, pageSize, pageNum) => {
+    return request({
+        url: baseurl + "/admin/page?pageSize=" + pageSize + "&pageNum=" + pageNum,
+        method: "post",
+        headers: headers,
+        data: data
+    })
+}
+//查询单个用户发布的发布的商品列表
+export const list = (uid) => {
+    return request({
+        url: baseurl + "/list/" + uid,
+        method: "get",
+    })
+}
+
+//用户修改商品信息
+export const update = (data) => {
+    return request({
+        url: baseurl + "/update",
+        method: "post",
+        headers: headers,
+        data: data
+    })
+}
